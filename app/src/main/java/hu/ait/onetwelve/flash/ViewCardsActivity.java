759b1c7d94fd7ac0f95aed4a2ce8a6e9280cb3f2
Brendan Friesen <brendangrub@gmail.com>
2016-12-11 16:07:41 +0100

    View shows example card.

---
+++ app/src/main/java/hu/ait/onetwelve/flash/ViewCardsActivity.java
@@ -0,0 +1,18 @@
+package hu.ait.onetwelve.flash;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Brendan on 12/11/16.
+ */
+
+public class ViewCardsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_cards);
+    }
+}
---
+++ app/src/main/java/hu/ait/onetwelve/flash/view/OnSwipeListener.java
@@ -0,0 +1,11 @@
+package hu.ait.onetwelve.flash.view;
+
+public interface OnSwipeListener {
+  void onSwipeLeft();
+
+  void onSwipeRight();
+
+  void onSwipeUp();
+
+  void onSwipeDown();
+}
